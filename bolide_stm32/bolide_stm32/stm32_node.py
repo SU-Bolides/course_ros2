68c3a8a111624ab20d077c52ce3829b713d923fa
Arthasee <baptiste.braun.delvoye@gmail.com>
2025-04-11 21:37:43 +0200

    cleaning stm32 node

--- bolide_stm32/bolide_stm32/stm32_node.py
+++ bolide_stm32/bolide_stm32/stm32_node.py
@@ -1,14 +1,17 @@
+"""stm32 node. It receives data of the sensors (IMU, infrared, fork) from the STM32 and send it the PWM for the propulsion motor."""
 
+# IMPORTS
 import threading
+import spidev
+import math
 
 import rclpy
 from rclpy.node import Node
 from rclpy.executors import ExternalShutdownException
-import spidev
 from std_msgs.msg import Float32MultiArray, Int16
 from sensor_msgs.msg import Range, Imu
 from bolide_interfaces.msg import ForkSpeed, MultipleRange
-import math
+
 
 def spin_in_background():
     executor = rclpy.get_global_executor()
@@ -17,61 +20,71 @@ def spin_in_background():
     except ExternalShutdownException:
         pass
 
+
 class STM32_Parser(Node):
     def __init__(self):
         super().__init__('stm32_node')
 
+        # STM32 set-up
         self.BAUDRATE = 112500
 
-        bus=0 
-        device=1
+        bus = 0
+        device = 1
 
+        self.spi = spidev.SpiDev()
+        self.spi.open(bus, device)
+        self.spi.mode = 0
+        self.spi.max_speed_hz = self.BAUDRATE
+
+        # Publishers
         self.stm_pub = self.create_publisher(Float32MultiArray, '/stm32_sensors', 10)
         self.speed_pub = self.create_publisher(ForkSpeed, '/raw_fork_data', 10)
         self.ranges_pub = self.create_publisher(MultipleRange, '/raw_rear_range_data', 10)
         self.imu_pub = self.create_publisher(Imu, '/raw_imu_data', 10)
 
-        self.get_cmd = self.create_subscription(Int16, '/stm32_data', self.get_command,10)
+        # Subscribers
+        self.get_cmd = self.create_subscription(Int16, '/stm32_data', self.get_command, 10)
 
-        self.spi = spidev.SpiDev()
-        self.spi.open(bus,device)
-        self.spi.mode = 0
-        self.spi.max_speed_hz = self.BAUDRATE 
+        # Values
+        self.sensor_data = Float32MultiArray()  # table for the sensors data (see sheet online)
 
+        # Battery TODO - check it
+        self.vbat = 0.  # tension in the battery but it is not used because (maybe) it is broken
 
-        self.vbat = 0.
-        self.yaw = 0.
-        self.ir_gauche = 0.
-        self.ir_droit = 0.
-        self.speed = 0.
-        self.distance_US = 0.
-
-        self.tx_buffer = [0] *8
-
-        self.acc_x = 0.
-        self.yaw_rate = 0.
-
-        self.sensor_data = Float32MultiArray() # table for the sensors data
-        self.fork_data = ForkSpeed()
-        self.imu_data = Imu()
-
-        self.imu_data.angular_velocity_covariance = [0,0,0,0,0,0,0,0,1e-2]
-        self.imu_data.linear_acceleration_covariance = [1e-1,0,0,0,0,0,0,0,0]
-        self.imu_data.orientation_covariance = [0,0,0,0,0,0,0,0,1e-2]
-        self.imu_data.header.frame_id = "base_link"
-
+        # Infrared sensors
         self.ir_min_range = 0.06
         self.ir_max_range = 0.3
+        self.ir_gauche = 0.  # distance from the left infrared sensor
+        self.ir_droit = 0.  # distance from the right infrared sensor
+
+        # Fork
+        self.speed = 0.  # speed from the fork
+        self.fork_data = ForkSpeed()  # Fork message type (see bolide_interfaces)
+
+        # Ultrasound sensor
+        self.distance_US = 0.  # distancefrom the ultrasound sensor
+
+        # IMU
+        self.yaw = 0.  # twist around the z axis (vertical)
+        self.acc_x = 0.  # acceleration in the x axis
+        self.yaw_rate = 0.  # rotation speed around the z-axis of the car
+        # Set the covariance of the IMU (surely from datasheet) TODO - check it
+        self.imu_data.angular_velocity_covariance = [0, 0, 0, 0, 0, 0, 0, 0, 1e-2]
+        self.imu_data.linear_acceleration_covariance = [1e-1, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.imu_data.orientation_covariance = [0, 0, 0, 0, 0, 0, 0, 0, 1e-2]
+        self.imu_data.header.frame_id = "base_link"
+        self.imu_data = Imu()  # IMU message type (see sheet online)
+
+        # Transmitter
+        self.tx_buffer = [0] * 8  # data that we'll transmit to the STM32
 
         self.sensors_init()
-    
-    def get_command(self, msg:Int16):
+
+    def get_command(self, msg: Int16):
         command = []
         cmded_bytes = msg.data
         command.append((cmded_bytes >> 8) & 0xFF)
         command.append(cmded_bytes & 0xFF)
-
-        # print("cmd", command)
 
         crc = self.crc32mpeg2(command)
 
@@ -80,20 +93,19 @@ class STM32_Parser(Node):
         command.append((crc >> 8) & 0xFF)
         command.append((crc) & 0xFF)
 
+        # This should be not too big.
         self.tx_buffer = command + [0] * 2
-        #This should be not too big.
 
-    
     # Definitely not the most efficient way of doing this, but we're transmitting 16 bytes so it's OK.
-    # Ideally we'd do all this in Cpp, but I don't think there is a spidev equivalent in Cpp, and it's 
-    # not like we're being limited by this script. 
-    def crc32mpeg2(self,buf, crc=0xffffffff):
+    # Ideally we'd do all this in Cpp, but I don't think there is a spidev equivalent in Cpp, and it's
+    # not like we're being limited by this script.
+    def crc32mpeg2(self, buf, crc=0xffffffff):
         for val in buf:
             crc ^= val << 24
             for _ in range(8):
                 crc = crc << 1 if (crc & 0x80000000) == 0 else (crc << 1) ^ 0x104c11db7
         return crc
-    
+
     def sensors_init(self):
         """Initialize the sensors settings and the MultiRange message"""
         self.multi_range_frame = MultipleRange()
@@ -110,7 +122,7 @@ class STM32_Parser(Node):
         self.multi_range_frame.ir_rear_right.radiation_type = Range.INFRARED
         self.multi_range_frame.ir_rear_right.min_range = self.ir_min_range
         self.multi_range_frame.ir_rear_right.max_range = self.ir_max_range
-        
+
         # Rear sonar sensor =============================================================
         # self.multi_range_frame.Sonar_rear = Range()
         # self.multi_range_frame.Sonar_rear.header.frame_id = "rear_sonar_range_frame"
@@ -121,25 +133,22 @@ class STM32_Parser(Node):
     def receiveSensorData(self):
         self.spi.writebytes(self.tx_buffer)
         data = self.spi.readbytes(20)
-        #data = self.spi.xfer2([0x45]*20)  # SPI happens simultaneously, so we need to send to receive. 
+        # data = self.spi.xfer2([0x45]*20)  # SPI happens simultaneously, so we need to send to receive.
 
-
-        if (not self.crc32mpeg2(data)):
-            self.vbat = (data[0] << 8) | data[1] # UNIT??? It's just an ADC, but I think it clips bc it's always 4095 lmao
-            self.yaw = ((data[2] << 8) | data[3])/-900. #rad
-            self.ir_gauche = (data[4] << 8) | data[5]
-            self.ir_droit = (data[6] << 8) | data[7] 
-            self.speed = 0.002*((data[8] << 8) | data[9]) #ms^-1 # We have to add a factor 2 for some reason, don't ask me why though. 
-            self.distance_US = 0.01*((data[10] << 8) | data[11]) #m
-            self.acc_x = 0.01*int.from_bytes([data[12],data[13]], byteorder='big', signed=True) #ms^-2
-            self.yaw_rate = int.from_bytes([data[14],data[15]], byteorder='big', signed=True)/900. #rads^-1
+        if not self.crc32mpeg2(data):
+            self.vbat = (data[0] << 8) | data[1]  # UNIT??? It's just an ADC, but I think it clips bc it's always 4095 lmao
+            self.yaw = ((data[2] << 8) | data[3])/-900.  # rad
+            self.ir_gauche = (data[4] << 8) | data[5]  # mV
+            self.ir_droit = (data[6] << 8) | data[7]
+            self.speed = 0.002*((data[8] << 8) | data[9])  # ms^-1 - We have to add a factor 2 for some reason, don't ask me why though.
+            self.distance_US = 0.01*((data[10] << 8) | data[11])  # m
+            self.acc_x = 0.01*int.from_bytes([data[12], data[13]], byteorder='big', signed=True)  # ms^-2
+            self.yaw_rate = int.from_bytes([data[14], data[15]], byteorder='big', signed=True)/900.  # rads^-1
 
             self.sensor_data.data = [self.yaw, self.speed, self.ir_gauche, self.ir_droit, self.distance_US, self.acc_x, self.yaw_rate]
 
             self.fork_data.speed = self.speed
             self.fork_data.header.stamp = self.get_clock().now().to_msg()
-
-
 
             ##########################  RANGES  #############################
 
@@ -153,7 +162,6 @@ class STM32_Parser(Node):
             # ... and with a bit of experimentation and test to adapt the value
             IR_rear_left /= 100 # conversion cm --> m
             IR_rear_left = min(max(IR_rear_left,0), self.ir_max_range)
-            
 
             # Right ===================================
             IR_rear_right = self.ir_droit/1000. # conversion mV --> V
@@ -181,17 +189,17 @@ class STM32_Parser(Node):
             self.imu_data.angular_velocity.z = self.yaw_rate
             # self.imu_data.linear_acceleration.x = self.acc_x + 0.043
 
-
-        #stamp = rospy.Time.now()
+        # stamp = rospy.Time.now()
             stamp = self.get_clock().now().to_msg()
             self.fork_data.header.stamp = stamp
             self.imu_data.header.stamp = stamp
             self.stm_pub.publish(self.sensor_data)
 
-        #f (rclpy.ok()):
+        # if (rclpy.ok()):
         self.speed_pub.publish(self.fork_data)
         self.ranges_pub.publish(self.multi_range_frame)
         self.imu_pub.publish(self.imu_data)
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -201,7 +209,7 @@ def main(args=None):
     thread.start()
 
     rate = stm32_node.create_rate(200)
-    while(rclpy.ok()):
+    while rclpy.ok():
         stm32_node.receiveSensorData()
         rate.sleep()
     rclpy.shutdown()
