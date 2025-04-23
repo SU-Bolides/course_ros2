8a3965ded28efef8f10beb6479898fa545dd7566
NGUYEN_Minh_Nhut <Minh_Nhut.Nguyen@etu.sorbonne-universite.fr>
2025-04-23 11:19:43 +0200

    Update 2 teleop_keyboard.py

--- bolide_teleop/bolide_teleop/teleop_keyboard.py
+++ bolide_teleop/bolide_teleop/teleop_keyboard.py
@@ -76,27 +76,21 @@ class KeyboardController(Node):
             else:
                 self.current_speed = -0.05 * coeff
         elif action == 'LEFT':
-            if self.current_direction > -1.0:
-                self.current_direction += -1.0
-            else:
+            if self.current_speed != 0.0:  # If we are moving forward or backward 
+                if self.current_direction > -1.0:
+                    self.current_direction -= 1.0
+                else:
+                    self.current_direction = -1.0
+            else:  # otherwise we only touch the direction
                 self.current_direction = -1.0
-            if self.current_speed ==0.0:
-                # No need to touch the speed if it's already different from 0 
-                #pass
-            #else:
-                # Make sure that we don't start by turning if the speed is at 0
-                self.current_direction=self.current_direction
         elif action == 'RIGHT':
-            if self.current_direction < 1.0:
-                self.current_direction += 1.0
+            if self.current_speed != 0.0:
+                if self.current_direction < 1.0:
+                    self.current_direction += 1.0
+                else:
+                    self.current_direction = 1.0
             else:
                 self.current_direction = 1.0
-            if self.current_speed ==0.0:
-                # No need to touch the speed if it's already different from 0 
-                #pass
-            #else:
-                # Make sure that we don't start by turning if the speed is at 0
-                self.current_direction=self.current_direction
         elif action == 'BRAKE':
             self.current_speed = 2.0 * coeff
         elif action == 'QUIT':
