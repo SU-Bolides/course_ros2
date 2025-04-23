93fecb949ced1b0395e149db55d66d3316909d8c
NGUYEN_Minh_Nhut <Minh_Nhut.Nguyen@etu.sorbonne-universite.fr>
2025-04-23 10:46:51 +0200

    Adding conditions to turn actions

--- bolide_teleop/bolide_teleop/teleop_keyboard.py
+++ bolide_teleop/bolide_teleop/teleop_keyboard.py
@@ -80,11 +80,23 @@ class KeyboardController(Node):
                 self.current_direction += -1.0
             else:
                 self.current_direction = -1.0
+            if self.current_speed !=0.0:
+                # No need to touch the speed if it's already different from 0 
+                pass
+            else:
+                # Make sure that we don't start by turning if the speed is at 0
+                self.current_speed=0.0
         elif action == 'RIGHT':
             if self.current_direction < 1.0:
                 self.current_direction += 1.0
             else:
                 self.current_direction = 1.0
+            if self.current_speed !=0.0:
+                # No need to touch the speed if it's already different from 0 
+                pass
+            else:
+                # Make sure that we don't start by turning if the speed is at 0
+                self.current_speed=0.0
         elif action == 'BRAKE':
             self.current_speed = 2.0 * coeff
         elif action == 'QUIT':
